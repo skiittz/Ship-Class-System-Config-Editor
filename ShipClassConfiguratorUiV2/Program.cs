af356b70db0f203e8c576b3016c39948e7b40c0d
skiittz <wspeichert@gmail.com>
2024-07-17 13:59:55 -0400

    add update checker

--- ShipClassConfiguratorUiV2/Program.cs
+++ ShipClassConfiguratorUiV2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GitHubUpdate;
 
 namespace ShipClassConfiguratorUiV2
 {
@@ -11,12 +12,24 @@ namespace ShipClassConfiguratorUiV2
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        ///
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
+            var checker = new UpdateChecker("skiittz", "Ship-Class-System-Config-Editor");
+            UpdateType update = checker.CheckUpdate().Result;
+            if (update != UpdateType.None)
+            {
+                var result = new UpdateNotifyDialog(checker).ShowDialog();
+                if (result == DialogResult.Yes)
+                {
+                    checker.DownloadAsset("ShipClassConfigurator.zip"); // opens it in the user's browser
+                }
+            }
         }
     }
 }
--- ShipClassConfiguratorUiV2/Properties/AssemblyInfo.cs
+++ ShipClassConfiguratorUiV2/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ShipClassConfiguratorUiV2")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("UI for setting up xml files that run OB/Hailfire's ship classification mod")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ShipClassConfiguratorUiV2")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.1.3.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
